581ee63bc51e802d80cd14c03ccc1d6b7b08df52
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-27 12:09:38 +0200

    Iniziata classe vacanza con attributi costruttore e primo validatore

--- src/jana60/Vacanza.java
+++ src/jana60/Vacanza.java
@@ -1,5 +1,36 @@
 package jana60;
 
+import java.time.LocalDate;
+
 public class Vacanza {
+
+	// Attributi
+	private String destinazione;
+	private int giornoInizio, meseInizio, annoInizio, giornoFine, meseFine, annoFine;
+
+	public Vacanza(String destinazione, int giornoInizio, int meseInizio, int annoInizio, int giornoFine, int meseFine,
+			int annoFine) throws Exception {
+		super();
+		this.destinazione = destinazione;
+		this.validDestinazione();
+		this.giornoInizio = giornoInizio;
+		this.meseInizio = meseInizio;
+		this.annoInizio = annoInizio;
+		this.giornoFine = giornoFine;
+		this.meseFine = meseFine;
+		this.annoFine = annoFine;
+	}
+
+	LocalDate dataInizio = LocalDate.of(annoInizio, meseInizio, giornoInizio);
+	LocalDate dataFine = LocalDate.of(annoFine, meseFine, giornoFine);
+
+	// Attributi di Validazione
+
+	public void validDestinazione() throws Exception {
+		if (destinazione.equals(null)) {
+			throw new Exception("Devi scrivere qualcosa nel campo destinazione!");
+
+		}
+	}
 
 }
